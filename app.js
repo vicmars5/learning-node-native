df0961d9ec25a21e928ffcfd9aadc2907f574e35
Victor Diaz <mars.vic5@gmail.com>
2018-02-16 18:41:48 -0600

    buffers works

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const { Total } = require('./build/Release/addon')
 const { total } = require('./main.js')
 
 const arr = (new Array(5000000)).map(() => Math.random((Math.random() * 50)))
-
+const buff = Buffer.from(arr)
 // `Hello` function returns a string, so we have to console.log
 console.time('c++')
-console.log(`length: ${arr.length}. Sum: ${Total(arr)}`)
+console.log(`length: ${arr.length}. Sum: ${Total(buff, buff.length)}`)
 console.timeEnd('c++')
 console.time('js')
 console.log(`length: ${arr.length}. Sum: ${total(arr)}`)
--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 // NAN_METHOD is a Nan macro enabling convenient way of creating native node functions
 // It takes a method's name as a param. By C++ convention, I used the Capital cased name
 NAN_METHOD(Total) {
-    v8::Local<v8::Array> arr = v8::Local<v8::Array>::Cast(info[0]);
+    // We will test how to to this with arrays
+    char* buff = (char*) node::Buffer::Data(info[0]->ToObject());
+    unsigned int size = info[1]->Uint32Value();
     int x = 0;
-    for (unsigned int i = 0; i < arr->Length(); i++) {
-        x += Nan::Get(arr, i).ToLocalChecked()->IntegerValue();
+
+    for (unsigned int i = 0; i < size; i++) {
+        x += buff[i];
     }
+
     v8::Local<v8::Integer> retVal = Nan::New(x);
     info.GetReturnValue().Set(retVal);
 }
